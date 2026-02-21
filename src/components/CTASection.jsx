9f58253aada13fceeba35b236ef4d3dc723c86e3
Bob <bobmasika@gmail.com>
2026-02-22 01:36:53 +0300

    HomePage Feat: CTA Section

---
+++ src/components/CTASection.jsx
@@ -0,0 +1,26 @@
+import { usePromptStore } from "../store/usePromptStore";
+
+const CTASection = () => {
+    const generatePrompt= usePromptStore((state) => state.generatePrompt);
+
+    return (
+        <section className="text-center py-20 bg-gray-100">
+            <h2 className="text-3xl font-bold mb-4">
+                Ready to Start Sketching?
+            </h2>
+
+            <p className="text-gray-600 mb-8">
+                Join thousands of urban sketchers using daily prompts to improve their skills.
+            </p>
+
+            <button 
+            onClick={generatePrompt}
+            className="bg-gradient-to-r from-purple-600 to-pink-500 text-white px-6 py-3 rounded-lg font-semibold hover:scale-105 transition"
+            >
+                Generate Your First Prompt
+            </button>
+        </section>
+    );
+};
+
+export default CTASection;
